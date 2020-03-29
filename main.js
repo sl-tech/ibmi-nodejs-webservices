cb7e2b5717ea0b2032602f1f5c772cb49ddd1900
Sammy Lo <sammylo_uk@hotmail.com>
2020-03-29 19:13:06 +0100

    more stuff

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const http = require('http')
+const fs = require('fs')
 const port = 9911
 
-const server = http.createServer(function(req,res) {
-	res.write('Hello Node')
-	res.end()
+const server = http.createServer(function(req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/html'})
+	fs.readFile('index.html', function(error, data){
+		if(error) {
+			res.writeHead(404)
+			res.write('Error: file not found')
+		} else{
+			res.write(data)
+		}
+		res.end()
+	})
+	
 })
 
 server.listen(port, function(error) {
